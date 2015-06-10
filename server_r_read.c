718e85d67337c6d4b637be8e26351f89266e5b5d
manoj-shetty <manoj3121kumar@gmail.com>
2015-06-10 17:09:58 +0530

    Updated patch of rudiFS /*modularized read function for validation */
    Signed-off-by: manoj-shetty <manoj3121kumar@gmail.com>

--- server_r_read.c
+++ server_r_read.c
@@ -21,7 +21,7 @@ ssize_t
 r_read1 (r_file *file , char *buffer , ssize_t size) {
         int fd = file->fd , ret = -1;
         ssize_t byts_read;
-
+        if  (is_open (file)) {
                 byts_read = read (fd , buffer , size);
                 if (byts_read == -1) {
                         ret = -1;
@@ -38,6 +38,55 @@ r_read1 (r_file *file , char *buffer , ssize_t size) {
                         goto out;
 
                 }
+        }
 out:
+        return ret;
+}
+
+int
+is_open (r_file *file)
+{
+        int flage = 0 , ret = 0;
+        /*Loop variables */
+        r_inode *ptr = NULL;
+        r_fd *fd_ptr = NULL;
+
+        /*Search the inode in the Open_Files_Table */
+        for (ptr = op_tab ; (ptr != NULL) &&
+        (ptr->inode_number != file->inode_number); ptr = ptr->next)
+                ;
+
+        if (ptr != NULL) {
+                if (ptr->inode_number != file->inode_number) {
+                        flage = 1;
+                        goto out;
+                }
+        } else if (ptr == NULL) {
+                flage = 1;
+                goto out;
+        }
+
+        /* If the inode is found on the Open_Files_Table then search for
+         * the fd*/
+        if (ptr != NULL)
+                for (fd_ptr = ptr->fd_list ; (fd_ptr->fd != file->fd) &&
+                (fd_ptr != NULL) ; fd_ptr = fd_ptr->next)
+                        ;
+        if (fd_ptr != NULL) {
+                if (fd_ptr->fd != file->fd) {
+                        flage = 1;
+                        goto out;
+                }
+        } else if (fd_ptr == NULL) {
+                flage = 1;
+                goto out;
+        }
+
+out:    if (flage != 1) {
+                if ((ptr->inode_number == file->inode_number) &&
+                (fd_ptr->fd == file->fd))
+                        ret = 1;
+        } else
+                ret = 0;
         return ret;
 }
